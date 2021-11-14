dfced550ed8ad53915f0f62372addcd5f3d4f562
Swords <33922797+SleepySwords@users.noreply.github.com>
2021-11-14 15:50:33 +1100

    fix: fixes snippet engine taking over sometimes

--- nvim/lua/completion/cmp.lua
+++ nvim/lua/completion/cmp.lua
@@ -1,6 +1,15 @@
 local cmp = require'cmp'
 local types = require('cmp.types')
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
 	sorting = {
 		priority_weight = 2,
@@ -44,6 +53,26 @@ cmp.setup({
 		end,
     },
     mapping = {
+		["<Tab>"] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+		    cmp.select_next_item()
+		  elseif vim.fn["vsnip#available"](1) == 1 then
+		    feedkey("<Plug>(vsnip-expand-or-jump)", "")
+		  elseif has_words_before() then
+		    cmp.complete()
+		  else
+		    fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+		  end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function()
+		  if cmp.visible() then
+		    cmp.select_prev_item()
+		  elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+		    feedkey("<Plug>(vsnip-jump-prev)", "")
+		  end
+		end, { "i", "s" }),
+
 		['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
 		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
 		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
@@ -52,28 +81,14 @@ cmp.setup({
 		    i = cmp.mapping.abort(),
 		    c = cmp.mapping.close(),
 		}),
-		['<S-Tab>'] = function(fallback)
-		    if cmp.visible() then
-			cmp.select_prev_item()
-		    else
-			fallback()
-		    end
-		end,
-		['<Tab>'] = function(fallback)
-		    if cmp.visible() then
-			cmp.select_next_item()
-		    else
-			fallback()
-		    end
-		end,
 		['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'vsnip' },
-	    { name = 'buffer' },
-		{ name = 'path' },
-		{ name = 'cmdline' }
+		-- { name = 'buffer' },
+		-- { name = 'path' },
+		-- { name = 'cmdline' }
 		-- { name = 'luasnip' }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
 		-- { name = 'snippy' }, -- For snippy users.
