867a14fc81e89c4923a9cd560ff5f1691b599e94
Ibby <33922797+SleepySwords@users.noreply.github.com>
2024-12-27 20:05:15 +1100

    feat: add ghostty config

--- sync.sh
+++ sync.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env fish
 
-set configs "borders" "nvim" "fish" "karabiner" "kitty" "sketchybar" "skhd" "yabai"
-for config in $configs
-	rm -rf $config
-	cp -r ~/.config/$config .
-	# cp ~/.gitconfig .
-	cp -r ~/Library/Application\ Support/lazygit/ ./lazygit
+set dot_configs "borders" "nvim" "fish" "karabiner" "kitty" "sketchybar" "skhd" "yabai" "starship.toml"
+set application_support_config "lazygit" "com.mitchellh.ghostty"
+for config in $dot_configs
+    rm -rf $config
+    cp -r ~/.config/$config .
+end
+for config in $application_support_config
+    rm -rf $config
+    cp -r ~/Library/Application\ Support/$config .
 end
 cp ~/.clangd .
-cp ~/.config/starship.toml .
